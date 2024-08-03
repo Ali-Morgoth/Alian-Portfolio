a6636f46bd9756e245408a1ddf4d5cee37e4b70e
Ali-Morgoth <alian.andahur.lml@gmail.com>
2024-08-02 21:15:29 -0400

    Updated environment variables and .gitignore

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,99 +1,93 @@
-import { NextResponse } from 'next/server'
- 
+import { NextResponse } from 'next/server';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export async function POST(request) {
-    require('dotenv').config()
-     const data=await request.json()
-    let nodemailer = require('nodemailer')
-    try{
-    const transporter = nodemailer.createTransport({
-      port: 465,
-      host: "smtp.gmail.com",
-      service:"gmail",
-      
-      auth: {
-        // user: process.env.EMAIL,
-        // pass: process.env.PASSWORD,
-        user: 'alian.andahur.lml@gmail.com',
-        pass: 'yfvzefqahagpgfrt'
-      },
-    })
-    
-    const info = await transporter.sendMail({
-      from: `"${data.name}"`, 
-      to: 'alian.andahur.lml@gmail.com',
-      subject: `Inquiry from Portafolio Alian`, 
-      text: "", 
-      html: `
-      <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-        }
-        .email-container {
-            max-width: 600px;
-            margin: 0 auto;
-            padding: 20px;
-            background-color: #fff;
-            border: 2px solid #48AEDD;
-            border-radius: 10px;
-        }
-      
-        p {
-            font-size: 20px;
-            line-height: 1.5;
-            margin-bottom: 10px;
-            border: 2px solid #48AEDD;
-            border-radius: 50px 20px;
-            background-color: #48AEDD;
-            color: white;
-            padding: 5px;
-            text-align: center;
-            font-weight: 900;
-        }
-        ul { 
-            list-style-type: none;
-            padding: 0;
-        }
-        li {
-            margin-bottom: 5px;
-        }
-        .signature {
-            font-style: italic;
-            font-size: 14px;
-        }
-      </style>
-</head>
-<body>
-    <div class="email-container">
+    const data = await request.json();
+    let nodemailer = require('nodemailer');
+    try {
+        const transporter = nodemailer.createTransport({
+            port: 465,
+            host: "smtp.gmail.com",
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD
+            },
+        });
 
-        <p>Alian Andahur</p>
-        <ul>
-            <li><strong>Name:</strong> ${data.name}</li>
-            <li><strong>Email:</strong> ${data.email}</li>
-            <li><strong>Phone:</strong> ${data.phoneNumber}</li>
-            <li><strong>Subject:</strong> ${data.subject}</li>
-            <li><strong>Message:</strong><br>${data.message}</li>
-        </ul>
-    </div>
-</body>
-</html>
+        const info = await transporter.sendMail({
+            from: `"${data.name}"`,
+            to: process.env.EMAIL,
+            subject: `Inquiry from Portafolio Alian`,
+            text: "",
+            html: `
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <style>
+                    body {
+                        font-family: Arial, sans-serif;
+                    }
+                    .email-container {
+                        max-width: 600px;
+                        margin: 0 auto;
+                        padding: 20px;
+                        background-color: #fff;
+                        border: 2px solid #48AEDD;
+                        border-radius: 10px;
+                    }
+                    p {
+                        font-size: 20px;
+                        line-height: 1.5;
+                        margin-bottom: 10px;
+                        border: 2px solid #48AEDD;
+                        border-radius: 50px 20px;
+                        background-color: #48AEDD;
+                        color: white;
+                        padding: 5px;
+                        text-align: center;
+                        font-weight: 900;
+                    }
+                    ul {
+                        list-style-type: none;
+                        padding: 0;
+                    }
+                    li {
+                        margin-bottom: 5px;
+                    }
+                    .signature {
+                        font-style: italic;
+                        font-size: 14px;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class="email-container">
+                    <p>Alian Andahur</p>
+                    <ul>
+                        <li><strong>Name:</strong> ${data.name}</li>
+                        <li><strong>Email:</strong> ${data.email}</li>
+                        <li><strong>Phone:</strong> ${data.phoneNumber}</li>
+                        <li><strong>Subject:</strong> ${data.subject}</li>
+                        <li><strong>Message:</strong><br>${data.message}</li>
+                    </ul>
+                </div>
+            </body>
+            </html>
+            `,
+        });
 
-      `, 
-    });
-    if(info.messageId){
-      return NextResponse.json({ error:false, message:"Message successfully sent! Thank you for contacting Alian Andahur" })
+        if (info.messageId) {
+            return NextResponse.json({ error: false, message: "Message successfully sent! Thank you for contacting Alian Andahur" });
+        }
+        return NextResponse.json({ error: true, message: 'Something went wrong please try again.' });
 
+    } catch (e) {
+        console.log(e);
+        return NextResponse.json({ error: true, message: 'Something went wrong please try again.' });
     }
-    return NextResponse.json({ error:true, message:'Something went wrong please try again.' })
-
-  }
-  catch(e){
-    console.log(e)
-    return NextResponse.json({ error:true, message:'Something went wrong please try again.' })
-
-  }
-  }
+}
