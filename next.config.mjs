a3f91eff5926c956bce8109a6d731ee89b81ca80
Ali-Morgoth <alian.andahur.lml@gmail.com>
2024-08-03 21:23:58 -0400

    Add font import configuration to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        },
+      });
+  
+      return config;
+    },
+  };
+  
+  export default nextConfig;
+  
