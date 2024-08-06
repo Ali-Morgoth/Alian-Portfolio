1ca5156ba39323194186fb3f9a084669a923e90a
Ali-Morgoth <alian.andahur.lml@gmail.com>
2024-08-06 18:40:02 -0400

    Countries and cities update

--- src/app/components/CountriesList/CountriesList.js
+++ src/app/components/CountriesList/CountriesList.js
@@ -1,10 +1,11 @@
-
-
-// src/app/components/CountriesList/CountriesList.js
 import React, { useEffect, useState } from "react";
 import { firestore } from "../../lib/firebase";
 import { collection, onSnapshot } from "firebase/firestore";
 import countries from "i18n-iso-countries";
+import enLocale from "i18n-iso-countries/langs/en.json";
+
+// Registrar el locale inglés para i18n-iso-countries
+countries.registerLocale(enLocale);
 
 const CountriesList = () => {
   const [countriesData, setCountriesData] = useState([]);
@@ -16,7 +17,18 @@ const CountriesList = () => {
         const countryData = [];
         querySnapshot.forEach((doc) => {
           const data = doc.data();
-          const countryName = countries.getName(data.countryCode, "en"); // Asegúrate de que countryCode es el campo correcto
+          let countryName;
+
+          // Si existe un countrycode, obtenemos el nombre del país utilizando i18n-iso-countries
+          if (data.countrycode) {
+            countryName = countries.getName(data.countrycode, "en");
+          } else if (data.country) {
+            // Si no existe countrycode, usamos el nombre del país directamente de los datos
+            countryName = data.country;
+          } else {
+            countryName = "Unknown"; // Manejar casos donde no hay countrycode ni country
+          }
+
           countryData.push({
             name: countryName,
             count: data.count,
@@ -34,11 +46,11 @@ const CountriesList = () => {
 
   return (
     <div className="p-4 w-full max-w-md mx-auto my-10 bg-white rounded-lg shadow-lg border-2 border-gray-300 mt-2 cursor-pointer hover:bg-[#476571] hover:shadow-xl hover:-translate-y-2 transform transition duration-500">
-    <div className="flex justify-between items-center mb-4">
-        <h2 className="text-sm font-sans" >Top Countries</h2>
-        <span className="text-sm font-sans ml-16">Visitors</span>
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-sm font-sans">Top Countries</h2>
+        <span className="text-sm font-sans ml-16 text-[#e91e63]">Visitors</span>
       </div>
-      <ul>
+      <ul className="max-h-32 overflow-y-auto">
         {countriesData.map((country, index) => (
           <li
             key={index}
@@ -48,7 +60,7 @@ const CountriesList = () => {
               <span>{index + 1}</span>
               <span>{country.name}</span>
             </div>
-            <span className="font-semibold">{country.count}</span>
+            <span className="font-semibold text-[#e91e63]">{country.count}</span>
           </li>
         ))}
       </ul>
